67a5d5e77c1fee452fd93192ae4296114e924e99
clerdson <clerdson@gmail.com>
2022-06-25 19:42:00 -0300

    back button

--- app/src/main/java/com/clerdsonjuca/sky/ui/MainActivity2.kt
+++ app/src/main/java/com/clerdsonjuca/sky/ui/MainActivity2.kt
@@ -1,11 +1,12 @@
 package com.clerdsonjuca.sky.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import coil.load
-import com.clerdsonjuca.sky.R
 import com.clerdsonjuca.sky.databinding.ActivityMain2Binding
 import kotlinx.android.synthetic.main.activity_main2.*
+
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var _binding:ActivityMain2Binding
@@ -13,10 +14,21 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(_binding.root)
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true);
 
         val name = intent.getStringExtra("name")
         val image = intent.getStringExtra("image")
         _binding.imageView.load(image)
         _binding.textView2.text = name
+
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
