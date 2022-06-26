18dea60627af46cd36eff98487833d87fb030cf2
clerdson <clerdson@gmail.com>
2022-06-26 02:18:59 -0300

    check connectivity wifi

--- app/src/main/java/com/clerdsonjuca/sky/ui/MainActivity.kt
+++ app/src/main/java/com/clerdsonjuca/sky/ui/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.clerdsonjuca.sky.ui
 
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.clerdsonjuca.sky.R
@@ -27,18 +35,19 @@ class MainActivity : AppCompatActivity() {
 
         setupRecyclerview()
         viewModel.getAll()
-        viewModel.response.observe(this){
-            when(it){
-                is NetworkResult.Success->{
+        viewModel.response.observe(this) {
+            when (it) {
+                is NetworkResult.Success -> {
                     it.data?.let {
-                    myAdapter.setData(it) }
+                        myAdapter.setData(it)
+                    }
                 }
                 is NetworkResult.Error -> {
-                   Toast.makeText(this,"Error",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
                 }
                 is NetworkResult.Loading -> {
                     // show a progress bar
-                    Toast.makeText(this,"Carregando",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Carregando", Toast.LENGTH_SHORT).show()
                 }
             }
 //
@@ -50,8 +59,56 @@ class MainActivity : AppCompatActivity() {
 //            }else{
 //                println("error");
 //            }
+
+            val networkRequest = NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .addTransportType(NetworkCapabilities.TRANSPORT_WIFI)
+                .addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
+                .build()
+
+            val networkCallback = object : ConnectivityManager.NetworkCallback() {
+                // network is available for use
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                }
+
+                // Network capabilities have changed for the network
+                override fun onCapabilitiesChanged(
+                    network: Network,
+                    networkCapabilities: NetworkCapabilities
+                ) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
+                    val unmetered =
+                        networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED)
+                }
+
+                // lost network connection
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+
+                    val builder = AlertDialog.Builder(this@MainActivity)
+                    builder.setTitle("Alerta.")
+                    builder.setMessage("Este app precisa de internet")
+                    builder.setPositiveButton("Sim") { dialog, which ->
+                        Toast.makeText(this@MainActivity, "ok.", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
+                }
+            }
+
+                val connectivityManager =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        getSystemService(ConnectivityManager::class.java) as ConnectivityManager
+                    } else {
+                        TODO("VERSION.SDK_INT < M")
+                    }
+            connectivityManager.requestNetwork(networkRequest, networkCallback)
+            }
         }
-    }
+
     private fun setupRecyclerview() {
 
         myRecyclerView.adapter = myAdapter
